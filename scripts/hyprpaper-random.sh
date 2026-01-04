6055fbfd0afba610370f18d9de8ddc73579bc05b
Ramil Khasanshin <khasanshin.ramil@list.ru>
2026-01-04 16:55:09 +0300

    fix(hyprland): new version bs

--- scripts/hyprpaper-random.sh
+++ scripts/hyprpaper-random.sh
@@ -5,4 +5,5 @@ CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
-while ! hyprctl hyprpaper reload ,"$WALLPAPER"; do sleep 1; done
+hyprctl hyprpaper preload "$WALLPAPER"
+while ! hyprctl hyprpaper wallpaper ,"$WALLPAPER"; do sleep 1; done
